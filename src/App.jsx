08bb6bd6be1455256e67418955991cc43539927f
Yen-Chun,Lin <asd900606@gmail.com>
2025-10-25 17:09:16 +0800

    feat: add iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
     <>
-      <h1>ㄐㄐ</h1>
+      <iframe
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=B4mpZ4MxKVg6wf1b&rel=0&autoplay=1&mute=1&loop=1&playlist=dQw4w9WgXcQ"
+        title="YouTube video player"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
     </>
   );
 }
