8cd2fd73b2e7d612199499573c0e9093c81b9c60
Yen-Chun,Lin <asd900606@gmail.com>
2025-10-25 22:28:30 +0800

    feat: add a link to github repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 function App() {
   return (
     <>
+      <a
+        target="_blank"
+        className=""
+        href="https://github.com/JuneLin2001/fake.10000.gov.tw?tab=readme-ov-file#%E5%81%87%E8%A3%9D%E8%87%AA%E5%B7%B1%E6%98%AF-govtw-%E7%B6%B2%E5%9F%9F%E7%9A%84%E6%B8%AC%E8%A9%A6%E7%B6%B2%E7%AB%99"
+      >
+        Q: 為啥可以用 .gov
+      </a>
       <iframe
         src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=B4mpZ4MxKVg6wf1b&rel=0&autoplay=1&mute=1&loop=1&playlist=dQw4w9WgXcQ"
         title="YouTube video player"
